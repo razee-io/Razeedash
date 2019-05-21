777a8c4670197418dc61a1f018ac7a63cb4d7bf3
Ryan Graham <rmgraham@us.ibm.com>
2019-05-21 17:02:54 -0400

    allowed optionally setting an api url via configmap

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -55,6 +55,7 @@ Meteor.startup(()=>{
     Meteor.settings.public.GITHUB_URL = process.env.GITHUB_URL || 'https://github.com/';
     Meteor.settings.public.GITHUB_API = process.env.GITHUB_API || 'https://api.github.com/';
     Meteor.settings.public.RAZEE_GHE_URL = 'https://github.com/razee-io/razeedash';
+    Meteor.settings.public.RAZEEDASH_API_URL = process.env.RAZEEDASH_API_URL || null;
 
     const versionInfo = {
         buildId: process.env.BUILD_ID || '$$BUILD_ID$$',

68bf56c294cff4fdc54858ae40df4df196eb65b0
Dale R. Hille <dalehille@gmail.com>
2020-03-03 14:27:13 -0500

    add generate api key link if no api key was found

--- imports/ui/pages/deployables/index.js
+++ imports/ui/pages/deployables/index.js
@@ -2,4 +2,13 @@
 import './subscriptions/index.js';
 import './channels/index.js';
 import './page.html';
+import './page.scss';
 import './apiHelp.html';
+import { Meteor } from 'meteor/meteor';	
+import { Template } from 'meteor/templating';	
+
+Template.api_example.events({	
+    'click .js-generate-key': function() {	
+        Meteor.call('generateApikey');	
+    }	
+});	

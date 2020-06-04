8ff228c1ee434c685b9c20d757e492be2ea10865
Dale R. Hille <dalehille@gmail.com>
2020-06-04 07:29:35 -0400

    updating ClusterSubscription help modals with graphql example

--- imports/ui/pages/deployables/index.js
+++ imports/ui/pages/deployables/index.js
@@ -5,9 +5,16 @@ import './page.html';
 import './apiHelp.html';
 import { Meteor } from 'meteor/meteor';	
 import { Template } from 'meteor/templating';	
+import { Session } from 'meteor/session';
 
 Template.api_example.events({	
     'click .js-generate-key': function() {	
         Meteor.call('generateApikey');	
     }	
 });	
+
+Template.api_example.helpers({
+    orgId() {
+        return Session.get('currentOrgId');
+    }
+});
